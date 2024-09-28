cfd92947ff85cebb46e072888c73322842354af8
vet1ments <vet1ments@naver.com>
2024-09-28 23:53:53 +0000

    v2.0.41 update

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import find_packages, setup
 setup(
 name="grpc-oauth-vet1ments",
-version="2.0.40",
+version="2.0.41",
 description="assistant proto",
 long_description="s",
 long_description_content_type="text/markdown",
