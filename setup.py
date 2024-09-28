95a40b57548ee244d138306a9ec00ffb8e5a4135
vet1ments <vet1ments@naver.com>
2024-09-28 23:34:02 +0000

    v2.0.40 update

--- setup.py
+++ setup.py
@@ -1,12 +1,13 @@
 from setuptools import find_packages, setup
 setup(
 name="grpc-oauth-vet1ments",
-version="2.0.39",
+version="2.0.40",
 description="assistant proto",
 long_description="s",
 long_description_content_type="text/markdown",
 author="vet1ments",
-author_email="https://github.com/vet1ments/grpc-oauth-python.git",
+author_email="vet1ments@naver.com",
+url="https://github.com/vet1ments/grpc-oauth-python.git",
 packages=find_packages(exclude=[]),
 python_requires=">=3.11",
 )
