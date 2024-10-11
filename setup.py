cd1c977d264e7c77a02160988e1a5e49cdc38a21
vet1ments <vet1ments@naver.com>
2024-10-11 15:46:46 +0000

    v0.1.17 update

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import find_packages, setup
 setup(
 name="grpc-oauth-vet1ments",
-version="0.1.16",
+version="0.1.17",
 description="assistant proto",
 long_description="s",
 long_description_content_type="text/markdown",
